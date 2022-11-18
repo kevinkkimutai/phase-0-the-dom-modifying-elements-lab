021278b5d615129842921bf102f58c64378fa956
kevinkkimutai <kiruikevinkim@gmail.com>
2022-11-18 15:45:38 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const main = document.getElementById("main")
+main.remove()
+const newHeader = document.createElement("h1")
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Kelvin Kimutai is the champion"
+Collapse
+
+
+
+
+
+
+
+
+
+
